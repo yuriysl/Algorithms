5b621a6d9f3016808250d0fa2b741ea1115a3e6c
yuriysl <yuriysl14@gmail.com>
2015-09-25 18:27:41 +0300

    fgh

---
+++ SRM548/KingdomAndTrees.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRM548.KingdomAndTrees
+{
+	public class KingdomAndTrees
+	{
+		public int miLnevel(int[] heights)
+		{
+			int n = heights.Length;
+			long[] h = new long[n];
+			for (int i = 0; i < n; ++i) h[i] = heights[i];
+			long left = -1;
+			long right = (long)1e10;
+			while (right - left > 1)
+			{
+				long middle = (left + right) / 2;
+				long min = 0;
+				bool ok = true;
+				for (int i = 0; i < n; ++i)
+				{
+					min = Math.Max(min + 1, h[i] - middle);
+					if (min > h[i] + middle)
+						ok = false;
+				}
+				if (ok)
+					right = middle;
+				else
+					left = middle;
+			}
+			return (int)right;
+		}
+	}
+}
---
+++ SRMTests/SRM548/KingdomAndTreesTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRM548.KingdomAndTrees;
+
+namespace SRMTests.SRM548
+{
+	[TestClass]
+	public class KingdomAndTreesTests
+	{
+		[TestMethod]
+		public void minLevel1()
+		{
+			var kingdomAndTrees = new KingdomAndTrees();
+			int result = kingdomAndTrees.miLnevel(new[] {9, 5, 11});
+			Assert.AreEqual(3, result);
+		}
+
+		[TestMethod]
+		public void minLevel2()
+		{
+			var kingdomAndTrees = new KingdomAndTrees();
+			int result = kingdomAndTrees.miLnevel(new[] { 5, 8 });
+			Assert.AreEqual(0, result);
+		}
+
+		[TestMethod]
+		public void minLevel3()
+		{
+			var kingdomAndTrees = new KingdomAndTrees();
+			int result = kingdomAndTrees.miLnevel(new[] { 1, 1, 1, 1, 1 });
+			Assert.AreEqual(4, result);
+		}
+
+		[TestMethod]
+		public void minLevel4()
+		{
+			var kingdomAndTrees = new KingdomAndTrees();
+			int result = kingdomAndTrees.miLnevel(new[] { 548, 47, 58, 250, 2012 });
+			Assert.AreEqual(251, result);
+		}
+	}
+}
