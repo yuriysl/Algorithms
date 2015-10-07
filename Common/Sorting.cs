05dc81946dd8aa942d74eafb695b9e90986dbef5
yuriysl <yuriysl14@gmail.com>
2015-10-07 17:41:24 +0300

    InsertionSort

---
+++ Common/Sorting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+	public class Sorting
+	{
+		public void InsertionSort<T>(T[] a)
+			where T : IComparable<T>
+		{
+			int n = a.Length;
+			for (int j = 1; j < n; j++)
+			{
+				T key = a[j];
+				int i = j - 1;
+				while (i >= 0 && key.CompareTo(a[i]) < 0)
+				{
+					a[i + 1] = a[i];
+					i--;
+				}
+				a[i + 1] = key;
+			}
+		}
+	}
+}
---
+++ SRMTests/Common/SortingTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Tests
+{
+	[TestClass()]
+	public class SortingTests
+	{
+		[TestMethod()]
+		public void InsertionSortTest()
+		{
+			var sorting = new Sorting();
+			int[] input = { };
+			sorting.InsertionSort(input);
+			Assert.IsTrue(input.Length == 0);
+		}
+
+		[TestMethod()]
+		public void InsertionSortTest1()
+		{
+			var sorting = new Sorting();
+			int[] input = {1, 2};
+			sorting.InsertionSort(input);
+			Assert.IsTrue(input[0] == 1);
+			Assert.IsTrue(input[1] == 2);
+		}
+
+		[TestMethod]
+		public void InsertionSortTest2()
+		{
+			var sorting = new Sorting();
+			int[] input = { 2, 1 };
+			sorting.InsertionSort(input);
+			Assert.AreEqual(1, input[0]);
+			Assert.AreEqual(2, input[1]);
+		}
+
+		[TestMethod]
+		public void InsertionSortTest3()
+		{
+			var sorting = new Sorting();
+			int[] input = { 3, 1, 2 };
+			sorting.InsertionSort(input);
+			Assert.AreEqual(1, input[0]);
+			Assert.AreEqual(2, input[1]);
+			Assert.AreEqual(3, input[2]);
+		}
+	}
+}
