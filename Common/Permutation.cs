86be3b4ce0fc6cf0934be044852241af368e7084
yuriysl <yuriysl14@gmail.com>
2015-11-10 11:41:19 +0200

    permutations

---
+++ Common/Permutation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+	public class Permutation
+	{
+		public IEnumerable<BaseNode<TKey, TValue>[]> GetPremutations<TKey, TValue>(BaseNode<TKey, TValue>[] a, int n)
+		{
+			int m = a.Length;
+			if (n == 2)
+			{
+				yield return a;
+				NodeHelper<TKey, TValue>.SwapWithIndex(a[m - 2], a[m - 1]);
+				yield return a;
+			}
+			else
+			{
+				var p = GetPremutations(a, n - 1);
+				foreach (var pItem in p)
+				{
+					for (int i = m - n; i < m; i++)
+					{
+						NodeHelper<TKey, TValue>.SwapWithIndex(a[m - n], a[i]);
+						yield return pItem;
+						NodeHelper<TKey, TValue>.SwapWithIndex(a[m - n], a[i]);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ SRMTests/Common/PermutationTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Tests
+{
+	[TestClass()]
+	public class PermutationTests
+	{
+		[TestMethod()]
+		public void GetPremutationsTest()
+		{
+			var permutation = new Permutation();
+			var input = new BaseNode<int, object>[6] {
+				new BaseNode<int, object>(0, 0),
+				new BaseNode<int, object>(0, 1),
+				new BaseNode<int, object>(0, 2),
+				new BaseNode<int, object>(0, 3),
+				new BaseNode<int, object>(0, 4),
+				new BaseNode<int, object>(0, 5)
+			};
+			var permutations = permutation.GetPremutations(input, input.Length);
+			int i = 0;
+			foreach (var item in permutations)
+			{
+				Console.WriteLine("[Permutation:{0}], [{1}]", i, string.Join(",", item.Select(node => node.Key)));
+				i++;
+			}
+			var groups = permutations.GroupBy(item => string.Join(",", item.Select(node => node.Key)), item => item);
+			i = 0;
+			foreach (var group in groups)
+			{
+				Console.WriteLine("[Permutation Group:{0}], [Key:{1}], [Count:{2}]", i, group.Key, group.Count());
+				i++;
+			}
+		}
+	}
+}
