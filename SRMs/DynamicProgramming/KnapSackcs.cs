4e6575fa45fde249beef6f275d511860de4a2377
yuriysl <yuriysl14@gmail.com>
2016-04-02 20:15:57 +0300

    16.2.

--- SRMs/DynamicProgramming/KnapSackcs.cs
+++ SRMs/DynamicProgramming/KnapSackcs.cs
@@ -23,40 +23,109 @@ namespace Algorithms.SRMs.DynamicProgramming
 		public void CalculateMaxCostDiscrete(int[] u, int[] w, int wK, int[,] costs, int[,] weights)
 		{
 			int n = u.Length;
-			for (int i = 1; i <= n; i++)
-			{
-				if (w[i - 1] <= wK)
-				{
-					costs[i, i] = u[i - 1];
-					weights[i, i] = w[i - 1];
-				}
-			}
 
-			for (int len = 2; len <= n; len++)
+			for (int len = 1; len <= n; len++)
 			{
 				for (int i = 1; i <= n - len + 1; i++)
 				{
 					int j = i + len - 1;
-					for (int k = 1; k < len; k++)
+					for (int k = 1; k <= len; k++)
 					{
-						var cost = costs[i, i + k - 1] + u[j - 1];
-						var weight = weights[i, i + k - 1] + w[j - 1];
-						if (weight <= wK && cost > costs[i, j])
+						var cost = u[i + k - 2];
+						var weight = w[i + k - 2];
+						int subCost = 0;
+						int subWeight = 0;
+						if (k > 1)
 						{
-							costs[i, j] = cost;
-							weights[i, j] = weight;
+							subCost += costs[i, i + k - 2];
+							subWeight += weights[i, i + k - 2];
+						}
+						if (k < len)
+						{
+							subCost += costs[i + k, j];
+							subWeight += weights[i + k, j];
+						}
+						if (subWeight <= wK && subCost > costs[i, j])
+						{
+							costs[i, j] = subCost;
+							weights[i, j] = subWeight;
+						}
+						if (weight + subWeight <= wK && cost + subCost > costs[i, j])
+						{
+							costs[i, j] = cost + subCost;
+							weights[i, j] = weight + subWeight;
 						}
 					}
 				}
 			}
-
 		}
 
 		public double GetMaxCostFractional(int[] u, int[] w, int wK)
 		{
 			int n = u.Length;
-			double[,] costs = new double[n, n];
-			return costs[0, n - 1];
+			double[,] costs = new double[n + 1, n + 1];
+			double[,] weights = new double[n + 1, n + 1];
+			int[] optimals = new int[n + 1];
+
+			CalculateMaxCostFractional(u, w, wK, costs, weights);
+
+			return costs[1, n];
+		}
+
+		public void CalculateMaxCostFractional(int[] u, int[] w, int wK, double[,] costs, double[,] weights)
+		{
+			int n = u.Length;
+
+			for (int len = 1; len <= n; len++)
+			{
+				for (int i = 1; i <= n - len + 1; i++)
+				{
+					int j = i + len - 1;
+					for (int k = 1; k <= len; k++)
+					{
+						var cost = u[i + k - 2];
+						double weight = w[i + k - 2];
+						double subCost = 0;
+						double subWeight = 0;
+						if (k > 1)
+						{
+							subCost += costs[i, i + k - 2];
+							subWeight += weights[i, i + k - 2];
+						}
+						if (k < len)
+						{
+							subCost += costs[i + k, j];
+							subWeight += weights[i + k, j];
+						}
+						if (subCost > costs[i, j])
+						{
+							if (subWeight <= wK)
+							{
+								costs[i, j] = subCost;
+								weights[i, j] = subWeight;
+							}
+						}
+						if (cost + subCost > costs[i, j])
+						{
+							if (weight + subWeight <= wK)
+							{
+								costs[i, j] = cost + subCost;
+								weights[i, j] = weight + subWeight;
+							}
+							else if(wK >= subWeight)
+							{
+								var ratio = (wK - subWeight) / weight;
+								var newCost = cost * ratio + subCost;
+								if (newCost > costs[i, j])
+								{
+									costs[i, j] = newCost;
+									weights[i, j] = weight * ratio + subWeight;
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
